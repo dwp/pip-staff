9ebbc3bf4e25ed747775d9d9346c395dbb31f0e8
Richard Smith <richard.smith1@engineering.digital.dwp.gov.uk>
2021-08-10 17:04:19 +0100

    Addin v7 and starting new Private Beta screens

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,20 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/v7/option_1/new-existing-user', (req, res, next) => {
+    res.redirect('/v7/option_1/pip-service-choice');
+});
+
+
+
+router.post('/v7/option_1/pip-service-choice', (req, res, next) => {
+    const pipService = req.session.data['pip-service'];
+    if (pipService === 'New') {
+        res.redirect('/v7/option_1/claimant-search');
+    } else {
+        res.redirect('/v7/option_1/claimant-search-2');
+    }
+});
+
+
 module.exports = router
