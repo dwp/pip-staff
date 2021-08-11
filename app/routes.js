6aa01e1dcfe72109e396f69f468cd87ca32c1ba3
Richard Smith <richard.smith1@engineering.digital.dwp.gov.uk>
2021-08-11 14:56:51 +0100

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -3,20 +3,28 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-router.post('/v7/option_1/new-existing-user', (req, res, next) => {
-    res.redirect('/v7/option_1/pip-service-choice');
-});
+// router.post('/v7/option_1/new-existing-user', (req, res, next) => {
+//     res.redirect('/v7/option_1/pip-service-choice');
+// });
 
 
 
-router.post('/v7/option_1/pip-service-choice', (req, res, next) => {
-    const pipService = req.session.data['pip-service'];
-    if (pipService === 'New') {
-        res.redirect('/v7/option_1/claimant-search');
-    } else {
-        res.redirect('/v7/option_1/claimant-search-2');
-    }
-});
+// router.post('/v7/option_1/pip-service-choice', (req, res, next) => {
+//     const pipService = req.session.data['pip-service'];
+//     if (pipService === 'New') {
+//         res.redirect('/v7/option_1/claimant-search');
+//     } else {
+//         res.redirect('/v7/option_1/claimant-search-2');
+//     }
+// });
+
+// router.post('/v7/option_1/email-confirmation', (req, res, next) => {
+//     res.redirect('/v7/option_1/new-existing-user');
+// });
+
+// router.post('/v7/option_1/email-confirmation', (req, res, next) => {
+//     res.redirect('/v7/option_1/new-existing-user');
+// });
 
 
 module.exports = router
